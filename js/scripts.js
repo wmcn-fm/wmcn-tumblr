fcb370fd2d75162c701c16d5724745bf21838904
Will Kent-Daggett <willkentdaggett@gmail.com>
2013-08-11 17:49:16 -0500

    add smooth scroll

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	//SMOOTH SCROLL:
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 500, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	}); //end click
+
+
+}); // end ready

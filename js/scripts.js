4f94a49e006da69d41decbcc9bd4830788071c07
Will Kent-Daggett <willkentdaggett@gmail.com>
2013-09-25 19:15:00 +0200

    show tags on staff blog

--- js/scripts.js
+++ js/scripts.js
@@ -42,10 +42,10 @@ $(document).ready(function(){
 	//TUMBLR:
 
 	var counter = 1;
-	var tagCounter = counter - 1;
 
 	var prependTag1 = '<p class="post_tag"><a href="http://wmcn.tumblr.com/tagged/';
-	var appendTag = '</a></p><span> &bull; </span>';
+	var appendTag = '</a></p>';
+	var bullet = '<span> &bull; </span>';
 
 	$.ajax({
 		url: 'http://api.tumblr.com/v2/blog/wmcn.tumblr.com/posts/text?api_key=GwinmSoZc7RkdQ6kcfEHvHAZxQyu0lpj3t82v4EST0nrYE6B1e&limit=9',
@@ -60,22 +60,22 @@ $(document).ready(function(){
 
 		  		wholeTags = [];
 
+
 		  		for (var i = 0; i < tags.length; i++) {
 
-		  			var dashed = tags[i].replace(/ /g,'-').replace(/'/g,"").replace('.','') + '/">';
+		  			var dashed = tags[i].replace(/ /g,'-').replace(/'/g,"%27") + '/">';
 
-		  			wholeTag = prependTag1 + dashed + tags[i] + appendTag;
+		  			wholeTag = prependTag1 + dashed + tags[i] + appendTag + bullet;
+
 		  			wholeTags.push(wholeTag);
 				}
 
-
 		  		$('div#' + counter).first().append("<a href=" + post_url + "><h2>" + heading + "</a></h2>");
 		  		$('div#' + counter).first().append(content);
-		  		$('div#' + counter).first().append(wholeTags);
-		  		console.log(wholeTags[tagCounter]);
+		  		$('div#' + counter).first().append(wholeTags)
 
 		  		counter++;
-		  	});
+		  	}); //end each
 		}
 	});
 
@@ -90,9 +90,21 @@ $(document).ready(function(){
 				data = item.date,
 				heading = item.title,
 				content = item.body
+				tags = item.tags
+
+				wholeTags = [];
+
+		  		for (var i = 0; i < tags.length; i++) {
+
+		  			var dashed = tags[i].replace(/ /g,'-').replace(/'/g,"").replace('.','') + '/">';
+
+		  			wholeTag = prependTag1 + dashed + tags[i] + appendTag;
+		  			wholeTags.push(wholeTag);
+				}
 
 				$('div#b' + counter2).first().append("<a href=" + post_url + "><h2>" + heading + "</a></h2>");
 		  		$('div#b' + counter2).first().append(content);
+		  		$('div#b' + counter2).first().append(wholeTags);
 
 		  		counter2++;
 			}); // end each
